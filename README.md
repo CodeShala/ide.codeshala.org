0952a00fcaab5f6e1bee1532d1b2bdee3e0899cd
Vipin Khushu <vipinkhushu@hotmail.com>
2018-03-01 01:08:15 +0530

    Basic functionality ready

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# ide.codeshala.org
-Code Compile IDE of codeshala
+# ide.Codeshala.org | Code Compile IDE of codeshala 
+
+[![CODESHALA](http://codeshala.org/assets/images/site_logo.png)](http://codeshala.org/assets/images/site_logo.png)
+
+
+### Hosting Server
+
+* Staging Server: [HEROKU](heroku.com)
+* Production Server : [AWS](amazonaws.com)
+
+
---
+++ index.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var compile_run = require('compile-run');
+var app = express();
+
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/',(req, res)=>{
+	res.render('index');
+})
+
+app.post('/run',(req,res)=>{
+	//console.log(req.body.language+" "+req.body.code+" "+req.body.input);
+	var language=req.body.language;
+	var code=req.body.code;
+	var input=req.body.input;
+	if(language=="c"){
+		compile_run.runC(code, input, function (stdout, stderr, err) {
+			if(!err){
+				res.setHeader('Content-Type', 'application/json');
+    			res.send(JSON.stringify({ stdout: stdout,stderr: stderr}));
+			}
+			else{
+				res.setHeader('Content-Type', 'application/json');
+    			res.send(JSON.stringify({ stderr: err}));
+			}
+		});
+	}else if(language=="c++"){
+		compile_run.runCpp(code, input, function (stdout, stderr, err) {
+			if(!err){
+				res.setHeader('Content-Type', 'application/json');
+    			res.send(JSON.stringify({ stdout: stdout,stderr: stderr}));
+			}
+			else{
+				res.setHeader('Content-Type', 'application/json');
+    			res.send(JSON.stringify({ stderr: err}));
+			}
+		});
+	}else{
+		console.log('Language Not Supported')
+	}
+	//res.send("OK");
+})
+
+app.get('/*',(req, res)=>{
+	res.send('404 | Page Not Found');
+})
+
+app.listen(process.env.PORT||3000,function(){
+	console.log('SERVER LISTENING');
+})
